ba284d576f5abba78a3dca14d560136f6687307a
Daniel Gardiner <gardiner.daniel@hotmail.co.uk>
2017-03-22 19:24:17 +0000

    major changes to python script

--- number_guess.py
+++ number_guess.py
@@ -1,12 +1,38 @@
 
 # going to make a number guessing game
 
-x = 4
+#x = input("Please guess a number:")
+x = 799
 
 import random
 
-random_number = random.randint(0, 9)
+random_number = random.randint(10, 10000)
 
-print(x - random_number)
+random_number = 778
 
-print(x)
+if (x - random_number) == 0:
+    message1 = "CORRECT!"
+elif (x - random_number) < 0:
+    message1 = "HIGHER!"
+else:
+    message1 = "LOWER!"
+
+if abs(x - random_number) == 0:
+    message2 = " well done"
+elif abs(x - random_number) <= 10:
+    message2 = " but close..."
+elif abs(x - random_number) <= 20:
+    message2 = " sort of close..."
+else:
+    message2 = " (not even close!)"
+
+print(message1 + message2)
+
+
+#print((x - random_number) <= 1)
+
+#print(random_number)
+
+#print(x)
+
+#print(x - random_number)
