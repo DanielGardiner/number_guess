4829a6ec61bae5c0adda735926bbb6bae463e6a7
Daniel Gardiner <gardiner.daniel@hotmail.co.uk>
2017-03-24 14:06:22 +0000

    removed out needless correct sections

--- number_guess.py
+++ number_guess.py
@@ -10,18 +10,14 @@ random_number = random.randint(1, 20)
 #random_number = 778
 
 while (x - random_number) != 0:
-    if (x - random_number) == 0:
-        message1 = "CORRECT!"
-    elif (x - random_number) < 0:
+    if (x - random_number) < 0:
         message1 = "HIGHER!"
     else:
         message1 = "LOWER!"
 
-    if abs(x - random_number) == 0:
-        message2 = " well done"
-    elif abs(x - random_number) <= 10:
+    if abs(x - random_number) <= 5:
         message2 = " but close..."
-    elif abs(x - random_number) <= 20:
+    elif abs(x - random_number) <= 10:
         message2 = " sort of close..."
     else:
         message2 = " (not even close!)"
